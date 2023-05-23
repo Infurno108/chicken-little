a122ffc5aecb9de30fe82496b35e79b360369c82
Flint Rose <oliverflintrose@gmail.com>
2023-05-23 12:20:28 -0500

    Another road trip commit. Made a lot of progress.

--- chicken.py
+++ chicken.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import keras as kr
-timeSteps = 1000
-forSteps = abs(1000 - 2601)
-blocks = 125
-
+timeSteps = 1871
+forSteps = abs(timeSteps - 2601)
+blocks = 270
+maxSize = 10000
 data = []
 
 with open('appleTenYears.csv', newline='') as csvfile:
@@ -35,38 +35,92 @@ def showGraph(prices):
 
     plt.show()
 
-#def trainerBuilder(prices, column):
-#    for i in 
+def trainerBuilder(prices):#we need an array of: [days data, next days column data]
+    global maxSize 
+    returnArray = []
+    for i in range(1, len(prices)):
+        returnArray.append([prices[i][0]/maxSize,prices[i][1]/maxSize, prices[i][2]/maxSize, prices[i][3]/maxSize,prices[i][4]/maxSize])
+    return returnArray
 
 open = []
 close = []
 high = []
 low = []
+adj = []
+volume = []
 for i in range(1, len(data) - forSteps):
     open.append(float(data[i][1]))
     high.append(float(data[i][2]))
     low.append(float(data[i][3]))
     close.append(float(data[i][4]))
+    adj.append(float(data[i][5]))
+    volume.append(float(data[i][6]))
+
 prices = pd.DataFrame({
     'open': open,
     'close': close,
     'high': high,
-    'low': low},
+    'low': low,
+    'adj': adj},
     index = pd.date_range(str(data[1][0]), periods=timeSteps, freq="d")
 )
 
-
 #showGraph(prices)
 
-inputLayer = kr.Input(shape=(4,))
+inputs = trainerBuilder(prices.values)
+outputs = inputs[1:]
+inputs = inputs[:(timeSteps - 2)]
 
-openModel = kr.models.Sequential()
-openModel.add(kr.layers.LSTM(blocks, input_dim=6))
-openModel.add(kr.layers.Dense(1))
-openModel.build()
-openModel.summary()
+inputs = np.array(inputs)
+outputs = np.array(outputs)
+
+def trainAndBuild():
+    chicken = kr.models.Sequential()
+    chicken.add(kr.layers.Dense(blocks, input_shape=(5, 1)))
+    chicken.add(kr.layers.LSTM(blocks, return_sequences=True))
+    chicken.add(kr.layers.LSTM(blocks, return_sequences=True))
+    chicken.add(kr.layers.LSTM(blocks, return_sequences=True))
+    chicken.add(kr.layers.Dense(1))
+    chicken.compile(
+        loss='mean_squared_error',
+        optimizer="adam",
+    )	
+    chicken.summary()
+    chicken.fit(inputs, outputs, epochs=200, batch_size=32, verbose=2)
+    chicken.save("chicken.h5")
+
+def runTests(timeWacky):
+    difference = 0
+    totalDifference = 0
+    start = 0
+    chicken = kr.models.load_model('chicken.h5')
+    for i in range(0, timeWacky):
+        prediction = chicken.predict(inputs[i].reshape(1,5))
+        prediction = prediction*maxSize
+        real = outputs[i + start]*maxSize
+        print("prediction: \n", prediction[0], "\nactual: ", real)
+        predictScore = sum(prediction[0])
+        realScore = sum(real)
+        print("\nPredict Score: ", predictScore[0], "\nReal Score: ", realScore)
+        difference = abs(predictScore - realScore)
+        print("\nDifference: ", difference)
+        totalDifference = totalDifference + difference
+    print("\nAverage Difference: ", (totalDifference/timeWacky))
+
+chicken = kr.models.load_model('chicken.h5')    
+test = chicken.predict(inputs[0].reshape(1,5))
+print("\n Input: ", inputs[0] * maxSize)
+print("\n Prediction: ", test * maxSize)
+print("\n Expected: ", outputs[0] * maxSize)
+print("\n Difference: ",)
+
+
+
+#runTests(timeSteps - 2)
+#trainAndBuild()
 
 #chicken.add(kr.layers.Dense(30, input_dim=))
 
 
-
+#268: 2.488612
+#
